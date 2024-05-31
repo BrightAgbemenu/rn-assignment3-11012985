5ffbddb799d7c23c443f9d01ca053fdb291d2920
bright Agbemenu <bagbemenu@st.ug.edu.gh>
2024-05-31 18:03:48 +0000

    app.js containing ongoingtask component

--- my-app/App.js
+++ my-app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, ScrollView, TouchableOpacity, ScrollViewComponent } from 'react-native';
 
 export default function App() {
   return (
@@ -19,72 +19,115 @@ export default function App() {
 
       <View style={styles.searchContainer}>
         <TextInput style={styles.searchInput} placeholder="Search" />
-        <TouchableOpacity style={styles.filterButton}>
-          <Image source={require('./assets/Filter.jpg')} style={styles.filterIcon} />
+        <TouchableOpacity style={styles.filterButton}> 
+          <Image source={require('./assets/Filter.jpg')} style={styles.filterIcon} /> 
+          
         </TouchableOpacity>
       </View>
-
-      <Text style={styles.sectionTitle}>Categories</Text>
+      <Text style={styles.sectionTitle}>categories</Text>
       <ScrollView horizontal={true} style={styles.horizontalScroll} showsHorizontalScrollIndicator={false}>
-        <View style={styles.categoriesContainer}>
-          <TouchableOpacity style={styles.categoryCard}>
-            <Image
-              style={styles.categoryImage}
-              source={require('./assets/Gaming.png')}
-            />
-            <Text style={styles.categoryTitle}>Game</Text>
-            <Text style={styles.categoryTasks}>12 Tasks</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryCard}>
-            <Image
-              style={styles.categoryImage}
-              source={require('./assets/Eating.jpg')}
-            />
-            <Text style={styles.categoryTitle}>Eat</Text>
-            <Text style={styles.categoryTasks}>12 Tasks</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryCard}>
-            <Image
-              style={styles.categoryImage}
-              source={require('./assets/Skating.jpg')}
-            />
-            <Text style={styles.categoryTitle}>Skate</Text>
-            <Text style={styles.categoryTasks}>12 Tasks</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryCard}>
-            <Image
-              style={styles.categoryImage}
-              source={require('./assets/Exercise.png')}
-            />
-            <Text style={styles.categoryTitle}>Exercise</Text>
-            <Text style={styles.categoryTasks}>12 Tasks</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryCard}>
-            <Image
-              style={styles.categoryImage}
-              source={require('./assets/Paragliding.jpg')}
-            />
-            <Text style={styles.categoryTitle}>Paraglide</Text>
-            <Text style={styles.categoryTasks}>12 Tasks</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryCard}>
-            <Image
-              style={styles.categoryImage}
-              source={require('./assets/Wrestling-removebg-preview.png')}
-            />
-            <Text style={styles.categoryTitle}>Wrestle</Text>
-            <Text style={styles.categoryTasks}>12 Tasks</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryCard}>
-            <Image
-              style={styles.categoryImage}
-              source={require('./assets/Studying.png')}
-            />
-            <Text style={styles.categoryTitle}>Study</Text>
-            <Text style={styles.categoryTasks}>12 Tasks</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+      <View style={styles.categoriesContainer}>
+        <TouchableOpacity style={styles.categoryCard}>
+          <Image
+            style={styles.categoryImage}
+            source={require('./assets/Gaming.png')} />
+          <Text style={styles.categoryTitle}>Game</Text>
+          <Text style={styles.categoryTasks}>12 Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.categoryCard}>
+          <Image
+            style={styles.categoryImage}
+            source={require('./assets/Eating.jpg')} />
+          <Text style={styles.categoryTitle}>Eat</Text>
+          <Text style={styles.categoryTasks}>12 Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.categoryCard}>
+          <Image
+            style={styles.categoryImage}
+            source={require('./assets/Skating.jpg')} />
+          <Text style={styles.categoryTitle}>Skate</Text>
+          <Text style={styles.categoryTasks}>12 Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.categoryCard}>
+          <Image
+            style={styles.categoryImage}
+            source={require('./assets/Exercise.png')} />
+          <Text style={styles.categoryTitle}>Exercise</Text>
+          <Text style={styles.categoryTasks}>12 Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.categoryCard}>
+          <Image
+            style={styles.categoryImage}
+            source={require('./assets/Paragliding.jpg')} />
+          <Text style={styles.categoryTitle}>Paraglide</Text>
+          <Text style={styles.categoryTasks}>12 Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.categoryCard}>
+          <Image
+            style={styles.categoryImage}
+            source={require('./assets/Wrestling-removebg-preview.png')} />
+          <Text style={styles.categoryTitle}>Wrestle</Text>
+          <Text style={styles.categoryTasks}>12 Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.categoryCard}>
+          <Image
+            style={styles.categoryImage}
+            source={require('./assets/Studying.png')} />
+          <Text style={styles.categoryTitle}>Study</Text>
+          <Text style={styles.categoryTasks}>12 Tasks</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+      
+
+      <View style={styles.ongoingTasks}>
+        <Text style={styles.sectionTitle}>Ongoing Task</Text>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Mobile App Development</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Web Development</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Push Ups</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Exercise</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Study</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Data Structures</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Sofware Engineering</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Info Modelling</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Projects</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Cyber Security</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Canva</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>UI/UX</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Mock-Up Text</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>Timetable</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.taskCard}>
+          <Text style={styles.taskText}>After School Activities</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 }
@@ -121,12 +164,6 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 25,
   },
-  filterButton: {
-    marginLeft: 15,
-    padding: 12,
-    backgroundColor: '#F5F5F5',
-    borderRadius: 25,
-  },
   searchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -146,16 +183,19 @@ const styles = StyleSheet.create({
     borderColor: '#FFF',
     fontSize: 18,
   },
+  filterButton: {
+    marginLeft: 15,
+    padding: 12,
+    backgroundColor: '#F5F5F5',
+    borderRadius: 25,
+  },
   filterIcon: {
     width: 24,
     height: 24,
   },
-  horizontalScroll: {
+  
+  horizontalscroll: {
     flexGrow: 0,
-  },
-  sectionTitle :{
-    fontWeight:'bold',
-    fontSize: 20,
   },
   categoriesContainer: {
     flexDirection: 'row',
@@ -163,7 +203,7 @@ const styles = StyleSheet.create({
   },
   categoryCard: {
     width: '40%',
-    height: 250,
+    height:250,
     backgroundColor: '#FFF',
     padding: 10,
     borderRadius: 12,
@@ -184,5 +224,29 @@ const styles = StyleSheet.create({
   },
   categoryTasks: {
     color: '#888',
+  },
+  ongoingTasks: {
+    paddingHorizontal: 30,
+    marginTop: 20,
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+
+
+
+  taskCard: {
+    padding: 25,
+    marginVertical: 10,
+    backgroundColor: '#F5F5F5',
+    borderRadius: 15,
+    
+    height: 100,
+  },
+  taskText: {
+    fontSize: 20,
+
   },
 });
